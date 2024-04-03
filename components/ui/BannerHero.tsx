88aaa67f51e4f46b32f7718cd1a88f680e27b844
wesley moraes <wesleymoraesserafim@gmail.com>
2024-04-03 17:20:31 -0300

    feat: images to component banner hero

--- components/ui/BannerHero.tsx
+++ components/ui/BannerHero.tsx
@@ -1,40 +1,35 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
-import { Picture, Source } from "apps/website/components/Picture.tsx";
 
-export interface Banner {
-  /** @description desktop otimized image */
+interface Banner {
   desktop: ImageWidget;
-  /** @description mobile otimized image */
   mobile: ImageWidget;
-  /** @description Image's alt text */
   alt: string;
   isEmptyBrand?: boolean;
   action?: {
-    /** @description when user clicks on the image, go to this link */
     href: string;
-    /** @description Image text title */
     title: string;
-    /** @description Image text subtitle */
     subTitle: string;
-    /** @description Button label */
     label?: string;
-    /** @description Icon image */
-    icon?: string;
   };
-  /** @description Icon Label */
   icon?: ImageWidget[];
   label?: string;
 }
 
-export default function BannerHero(
-  { image, lcp, id }: { image: Banner; lcp?: boolean; id: string },
-) {
+export default function BannerHero({
+  image,
+  lcp,
+  id,
+}: {
+  image: Banner;
+  lcp?: boolean;
+  id: string;
+}) {
   const {
     alt,
     mobile,
     desktop,
     action,
-    icon,
+    icon = [],
     label,
     isEmptyBrand,
   } = image;
@@ -42,14 +37,13 @@ export default function BannerHero(
   return (
     <a
       id={id}
-      href={action?.href ?? "#"}
+      href={action?.href ?? '#'}
       aria-label={action?.label}
       className="relative overflow-y-hidden w-full"
     >
       {action && (
-        <div className="absolute top-[236px] md:bottom-0 bottom-1/2 lg:left-[138px] left-0 right-0 sm:right-auto  lg:max-w-[612px] flex flex-col justify-center gap-4 pl-8 py-12">
-          <span className={` ${isEmptyBrand ? "visual-brand w-[70px]" : ""}`}>
-          </span>
+        <div className="absolute top-[180px] md:bottom-0 bottom-1/2 lg:left-[138px] left-0 right-0 sm:right-auto lg:max-w-[612px] flex flex-col justify-center gap-4 pl-8 py-12">
+          <span className={`${isEmptyBrand ? 'visual-brand w-[70px]' : ''}`} />
           <span className="text-[21px] lg:text-[50px] font-bold text-white max-w-[260px] lg:max-w-[580px]">
             {action.title}
           </span>
@@ -59,28 +53,32 @@ export default function BannerHero(
           <span className="text-white text-xs mt-[60px]">
             <b>{label}</b>
           </span>
-          <div className="overflow-hidden">
-            <div className="flex items-center track-slider">
-              <span class="text-white">label 1</span>
-              <span class="text-white">label 1</span>
-              <span class="text-white">label 1</span>
-              <span class="text-white">label 1</span>
-              <span class="text-white">label 1</span>
-              <span class="text-white">label 1</span>
-              <span class="text-white">label 1</span>
-            </div>
+          <div className="flex items-center">
+            <ul class="scroll-container"
+              style="--animation-direction: normal; --animation-time: 25s; margin:0;">
+              <li class="scroll-items">
+                {icon &&
+                  icon.map((item, index) => (
+                    <img
+                      key={index}
+                      className="w-auto h-auto"
+                      src={item}
+                    />
+                  ))}
+              </li>
+            </ul>
           </div>
         </div>
       )}
       <img
         className="object-cover w-full h-full hidden lg:block"
-        loading={lcp ? "eager" : "lazy"}
+        loading={lcp ? 'eager' : 'lazy'}
         src={desktop}
         alt={alt}
       />
       <img
         className="object-cover lg:hidden"
-        loading={lcp ? "eager" : "lazy"}
+        loading={lcp ? 'eager' : 'lazy'}
         src={mobile}
         alt={alt}
       />
