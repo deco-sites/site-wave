94107ffd62ec8601eae0870b72ad191cedd6cf35
Wesley Moraes Serafim <wesleymoraesserafim@gmail.com>
2024-10-16 12:52:49 +0000

    Publish 5 from staging into origin/main

--- sections/Institutionals/Video.tsx
+++ sections/Institutionals/Video.tsx
@@ -3,43 +3,51 @@ import { useDevice } from "deco/hooks/useDevice.ts";
 
 
 export interface Props {
-    video: Video;
+  video: Video;
 }
 
 const AppVideo = ({ video }: Props) => {
-    const device = useDevice()
-    return (
-        <div
-            class="container px-5 lg:px-0 justify-center lg:justify-start">
-            {device === 'desktop' &&
-                <iframe
-                    width={785}
-                    height={500}
-                    class="rounded-lg"
-                    src={video}
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerpolicy="strict-origin-when-cross-origin"
-                    allowfullscreen>
-                </iframe>
+  const device = useDevice()
+  return (
+    <div
+      class="container px-5 lg:px-0 justify-center lg:justify-start">
+      {video &&
+        <>
+          {device === 'desktop' &&
+            <iframe
+              width={785}
+              height={500}
+              class="rounded-lg"
+              src={video}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen>
+            </iframe>
+          }
+        </>
+      }
+      {video &&
+        <>
+          {device === 'mobile' && video &&
+            <iframe
+              width={350}
+              height={219}
+              class="rounded-lg"
+              src={video}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen>
+            </iframe>
+          }
+        </>
+      }
 
-            }
-            {device === 'mobile' &&
-                <iframe
-                    width={350}
-                    height={219}
-                    class="rounded-lg"
-                    src={video}
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerpolicy="strict-origin-when-cross-origin"
-                    allowfullscreen>
-                </iframe>}
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default AppVideo
